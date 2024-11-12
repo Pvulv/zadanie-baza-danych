9c21d20654d50e1f0a75017912abb2c2b5f9e3d2
Paulina Wróbel <paulinawro13579@gmail.com>
2024-11-12 21:39:29 +0100

    Create table Clean_station and Clean_measure

--- baza_danych.py
+++ baza_danych.py
@@ -9,3 +9,28 @@ csv_file2 = 'clean_measure.csv'
 
 engine = create_engine('sqlite:///cleaning_stations.db')
 connection = engine.connect()
+
+meta = MetaData(bind=engine)
+
+Clean_stations = Table(
+    'Clean_stations', meta,
+    Column('station', String, primary_key = True),
+    Column('latitude', Float),
+    Column('longitude', Float),
+    Column('elevation', Float),
+    Column('name', String),
+    Column('country', String),
+    Column('state', String)
+)
+
+Clean_measure = Table(
+    'Clean_measure', meta,
+    Column('id', Integer, primary_key=True),
+    Column('station', String, ForeignKey('Clean_stations.station')),
+    Column('date', DATE),
+    Column('precip', Float),
+    Column('tobs', Integer),
+    UniqueConstraint('station', 'date', name='uix_station_date')
+)
+
+meta.create_all(engine)
